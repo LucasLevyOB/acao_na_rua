6f0c496a78e5915cd89a8a7ee0270383a3c746a7
Gabriel Ferreira <gabriel29@alu.ufc.br>
2023-06-20 12:48:37 -0300

    Ajustando config

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,47 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <div id="menu">
+	  <ul>
+		<li><a href="link1.htm"><v-icon color="warning" icon="mdi-alert" size="large"></v-icon></a></li>
+		<li><a href="link2.htm">link2</a></li>
+		<li><a href="link3.htm">link3</a></li>
+		<li><a href="link4.htm">link4</a></li>
+		<li><a href="link5.htm">link5</a></li>
+	  </ul>
+	</div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<style>
+
+.menu_lateral {
+  position: absolute;
+  width: 122px;
+  height: 930px;
+  left: 0px;
+  top: 93.23px;
+
+  filter: drop-shadow(7px 4px 6px rgba(0, 0, 0, 0.2));
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+.fundo_background{
+  position: absolute;
+  width: 121.53px;
+  height: 930px;
+  left: 0.18px;
+  top: 0px;
+
+  background: #8A2DD6;
+  box-shadow: 4px 0px 9px rgba(0, 0, 0, 0.25);
+  border-radius: 0px;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.itens_menu_1{
+  position: absolute;
+  width: 121.53px;
+  height: 55.85px;
+  left: 0.24px;
+  top: 166.95px;
 }
 </style>
