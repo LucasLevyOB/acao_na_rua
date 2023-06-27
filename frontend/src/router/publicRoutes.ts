8da76e311757590137b5451b5a4042ae7d7c8858
LucasLevyOB <oliveiralucaslevy@gmail.com>
2023-06-27 20:13:39 -0300

    feat: novas rotas publicas

--- frontend/src/router/publicRoutes.ts
+++ frontend/src/router/publicRoutes.ts
@@ -8,7 +8,22 @@ const publicRoutes = [
         path: '/login',
         name: 'Login',
         component: import("../pages/Login.vue"),
-    }
+    },
+    {
+        path: '/cadastrar',
+        name: 'Cadastrar',
+        component: import("../pages/Cadastrar.vue"),
+    },
+    {
+        path: '/sobre',
+        name: 'Sobre',
+        component: import("../pages/Sobre.vue"),
+    },
+    {
+        path: '/visitante',
+        name: 'Visitante',
+        component: import("../pages/Visitante.vue"),
+    },
 ];
 
 export default publicRoutes;
