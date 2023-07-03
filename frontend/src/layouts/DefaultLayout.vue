ac67c5d2aebc7dc2f65b4cc7069d4fae90963090
Gabriel Ferreira <ga2bri9elferreira@gmail.com>
2023-07-03 14:03:44 -0300

    Tirando menu de rotas

--- frontend/src/layouts/DefaultLayout.vue
+++ frontend/src/layouts/DefaultLayout.vue
@@ -3,15 +3,29 @@ import { ref } from 'vue';
 
 import Sidebar from '../components/Sidebar.vue';
 import Menu2 from '../components/Menu2.vue';
+import {useRouter} from 'vue-router';
+import { computed } from 'vue';
 
 const sidebarIsOpen = ref(false);
+const router = useRouter();
+
+router.getRoutes();
+router.currentRoute;
+
+
+const removeMenu = computed(()=>{
+    const loginRoute = '/login';
+    const boasVindasRoute = '/boas-vindas';
+    const currentPath = router.currentRoute.value.path;
+    return currentPath !== loginRoute && currentPath !== boasVindasRoute;
+})
 
 </script>
 
 <template>
     <main class="default-layout">
-        <Menu2 v-model:is-visible="sidebarIsOpen" />
-        <Sidebar v-model:is-visible="sidebarIsOpen" />
+        <Menu2 v-if="removeMenu" v-model:is-visible="sidebarIsOpen" />
+        <Sidebar v-if="removeMenu" v-model:is-visible="sidebarIsOpen" />
         <div class="content">
             <slot />
         </div>
